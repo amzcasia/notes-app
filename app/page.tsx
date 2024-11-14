2cda9530dac9ace11847358ed306a11d3798ea78
Anton-Mark Casia <130978181+amzcasia@users.noreply.github.com>
2024-11-14 16:14:14 +0800

    add save database to txt

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
 export default function Home(){
+
+  // async function saveDataToTxt() {
+
+  //   const res = await fetch('http://127.0.0.1:8090/api/collections/notes_app/records?page=1&perPage=30', { cache: 'no-store' });
+    
+  //   const data = await res.json();
+
+  //   const resToTxt = JSON.stringify(data, null, 2);
+
+  //   const fs = require('fs'); 
+
+  //   fs.writeFile('response.txt', resToTxt, (err : any) => {
+  //       if (err) {
+  //         console.error('Error writing to file', err);
+  //       } else {
+  //         console.log('Response saved to response.txt');
+  //       }
+  //     });
+
+  //   return 1;
+  // }
+
   return (
     <div>
       <h1>This is the Home Page</h1>
       <p>Some content
+
       </p>
     </div>
   );
